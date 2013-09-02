abf860ce7ab37bc8ab8cdae22dd8d8f3fb3518ba
Donald G Plugge <plugge@usa.net>
2013-09-02 18:11:33 -0400

    Initial Commit

---
+++ eManager.Web/Controllers/EmployeeController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using eManager.Web.Models;
+using eManager.Domain;
+
+namespace eManager.Web.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        private readonly IDepartmentDataSource _db;
+        public EmployeeController(IDepartmentDataSource db)
+        {
+            _db = db;
+        }
+        //
+        // GET: /Employee/
+        [HttpGet]
+        public ActionResult Create(int departmentId)
+        {
+            var model = new CreateEmployeeViewModel();
+            model.DepartmentId = departmentId;
+
+            return View(model);
+        }
+        [HttpPost]
+        public ActionResult Create(CreateEmployeeViewModel viewModel) 
+        {
+            if (ModelState.IsValid)
+            {
+                var department = _db.Departments.Single(d => d.Id == viewModel.DepartmentId);
+                var employee = new Employee();
+                employee.Name = viewModel.Name;
+                employee.HireDate = viewModel.HireDate;
+                department.Employees.Add(employee);
+                _db.Save();
+                return RedirectToAction("detail", "department", new { id = viewModel.DepartmentId });
+            }
+            return View(viewModel);
+        }
+    }
+}
---
+++ eManager.Web/Migrations/Configuration.cs
@@ -0,0 +1,30 @@
+namespace eManager.Web.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+    using eManager.Domain;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<eManager.Web.Infrastructure.DepartmentDb>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = true;
+        }
+
+        protected override void Seed(eManager.Web.Infrastructure.DepartmentDb context)
+        {
+
+            context.Departments.AddOrUpdate(d => d.Name,
+                new Department { Name = "Engineering"},
+                new Department { Name = "Sales"},
+                new Department { Name = "Human Resources"},
+                new Department { Name = "Shipping"}
+                );
+        }
+
+        
+        }
+    }
+
